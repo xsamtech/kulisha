eb89ab5bf485da97cf6dbadd2e011e995f562472
xsamtech <124665689+xsamtech@users.noreply.github.com>
2024-09-17 00:51:50 +0100

    Update notifications.php

--- resources/lang/fr/notifications.php
+++ resources/lang/fr/notifications.php
@@ -48,7 +48,7 @@ return [
     'transaction_done' => 'Votre opération est terminée !',
     'transaction_failed' => 'L’envoi de votre paiement a échoué',
     'transaction_request_failed' => 'Une erreur lors du traitement de votre requête',
-    'transaction_push_failed' => 'Impossible de traiter la demande, veuillez réessayer. Echec envoie du push',
+    'transaction_push_failed' => 'Impossible de traiter la demande, veuillez réessayer. Echec envoi du push',
     'transaction_type_error' => 'Veuillez choisir le type de transaction',
     'new_partner_message' => 'Vous pouvez maintenant vous connecter en tant que partenaire avec votre n° de téléphone. Mot de passe temportaire :',
     'reaction_sent' => 'Réaction envoyée',
